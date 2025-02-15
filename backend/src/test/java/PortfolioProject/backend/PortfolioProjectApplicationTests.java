889b316d9a709023ee2e21d13f7774e58bdd2b6f
albertasl <albertas.lukosevicius@gmail.com>
2025-02-15 11:50:35 +0200

    added backend BUGED

---
+++ backend/src/test/java/PortfolioProject/backend/PortfolioProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package PortfolioProject.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
